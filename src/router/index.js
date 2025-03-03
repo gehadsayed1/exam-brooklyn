428ab353f659a8d22ca473083fd11fe1b7a4fd26
Jihad <gehadsaed199@gmail.com>
2025-03-03 16:28:28 +0200

    add Notyf alert

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,15 @@
-
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Exam from '../views/Exam.vue';
 
 const routes = [
- { path: '/', component: Home },
- { path: '/exam', name: 'exam', component: Exam },
+  { path: '/', component: Home },
+  { path: '/exam', name: 'exam', component: Exam },
 ];
 
 const router = createRouter({
- history: createWebHistory(),
- routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
